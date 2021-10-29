2c8d57fa131a52675ebfb22854f3d938655d73b2
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-10-29 13:16:34 +0200

    dbConfig not push

--- php/DbConfig.php
+++ php/DbConfig.php
@@ -3,8 +3,8 @@ $local=1; //0 para la nube
 if ($local==1){
     $server="localhost";
     $user="root";
-    $pass="";
-    $basededatos="quiz";
+    $pass="2000";
+    $basededatos="db_G22";
 }
 else{
     $server="localhost";
