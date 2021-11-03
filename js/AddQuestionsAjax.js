a339b3232fd69276050faa6610c946aa9353956c
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-11-03 22:49:21 +0100

    tomaster

--- js/AddQuestionsAjax.js
+++ js/AddQuestionsAjax.js
@@ -1,17 +1,42 @@
-const form = document.getElementById('fquestion');
-form.addEventListener('submit', (event) => {
+//const form = document.getElementById('fquestion');
+$(document).ready(function() {
+  let form= $('#fquestion');
+$('#fquestion').on('submit', (event) => {
   event.stopPropagation();
   event.preventDefault();
 
-  const formData = new FormData(form);
+  const formData = new FormData(form[0]);
 
-  fetch('../php/AddQuestionWithImageAjax.php', {
+  
+  
+  /*fetch('../php/AddQuestionWithImageAjax.php', {
     method: 'post',
     body: formData,
-  }).catch((error) => {
-    console.error(error);
-  });
+  })
+    .then((response) => response.text())
+    .then((text) => {
+      console.log(text);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});*/
+$.ajax({
+  url: "../php/AddQuestionWithImageAjax.php",
+  type: "POST",
+  data: formData,
+  processData: false,
+  contentType: false
 });
+
+/*$.post('../php/AddQuestionWithImageAjax.php', formData, function (data) {
+  console.log(data.time);
+  
+} , "FormData");*/
+});
+})
+
+
 
 const addQuestion = (event) => {
   console.log(event);
