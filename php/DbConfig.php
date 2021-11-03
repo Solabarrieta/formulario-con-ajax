42443be797f6ecc194a62ba393a37b2ebea692bb
Oier <oieruni22@gmail.com>
2021-11-04 00:01:39 +0100

    Añadiendo los parametros para que funcione la BD en el server

--- php/DbConfig.php
+++ php/DbConfig.php
@@ -1,15 +1,13 @@
 <?php
-$local=1; //0 para la nube
-if ($local==1){
-    $server="localhost";
-    $user="root";
-    $pass="2000";
-    $basededatos="db_G22";
+$local = 0; //0 para la nube
+if ($local == 1) {
+    $server = "localhost";
+    $user = "root";
+    $pass = "2000";
+    $basededatos = "db_G22";
+} else {
+    $server = "localhost";
+    $user = "G22";
+    $pass = "TWTnlYm33HtAL";
+    $basededatos = "db_G22";
 }
-else{
-    $server="localhost";
-    $user="";
-    $pass="";
-    $basededatos="";
-}
-?>
